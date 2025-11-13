056902256c42b3622fe406f3de9246052f38cf05
falconerr88 <thiagolovey1@gmail.com>
2025-11-13 13:52:36 -0300

    Probando todo

--- app/main.py
+++ app/main.py
@@ -9,8 +9,7 @@ app.include_router(tasks.router, prefix="/tasks", tags=["Tasks"])
 
 @app.get("/")
 def home():
-    return {"message": "🚀 TaskBoard API corriendo correctamente en EC2
-                           Hecho por Thiago Lovey"}
+    return {"message": "🚀 TaskBoard API corriendo correctamente en EC2, Hecho por Thiago Lovey"}
 
 @app.post("/upload")
 async def upload_file(file: UploadFile = File(...)):
